c9010adebbb720c5b773b774c6ece7197bf2be29
Bram Adams <3282661+bramses@users.noreply.github.com>
2022-05-23 00:44:06 -0400

    editor-enhancements main manifest package-lock package settings styles

--- main.ts
+++ main.ts
@@ -1,48 +1,109 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, request } from 'obsidian';
+import { EditorExtensions } from "editor-enhancements";
+
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface GPT3SummarizerSettings {
+	apiKey: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: GPT3SummarizerSettings = {
+	apiKey: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class GPT3Summarizer extends Plugin {
+	settings: GPT3SummarizerSettings;
+
+
+	// // Custom hashid by @shabegom
+	// private createBlockHash(): string {
+	// 	let result = "";
+	// 	const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+	// 	const charactersLength = characters.length;
+	// 	for (let i = 0; i < 4; i++) {
+	// 		result += characters.charAt(
+	// 			Math.floor(Math.random() * charactersLength)
+	// 		);
+	// 	}
+	// 	return result;
+	// }
+
+	// async conveyorBelt(url = "") {
+	// 	// todo
+	// }
+
+	// async convertUrlToTitledLink(editor: Editor, url: string): Promise<void> {
+	// 	// Generate a unique id for find/replace operations for the title.
+	// 	const pasteId = `Fetching Answer#${this.createBlockHash()}`;
+
+	// 	// Instantly paste so you don't wonder if paste is broken
+	// 	editor.replaceSelection(`${pasteId}`);
+
+	// 	// Fetch title from site, replace Fetching Title with actual title
+	// 	const stackOverflowMarkdown = await this.conveyorBelt(url);
+
+	// 	const text = editor.getValue();
+
+	// 	const start = text.indexOf(pasteId);
+	// 	if (start < 0) {
+	// 		console.log(
+	// 			`Unable to find text "${pasteId}" in current editor, bailing out; link ${url}`
+	// 		);
+	// 	} else {
+	// 		const end = start + pasteId.length;
+	// 		const startPos = EditorExtensions.getEditorPositionFromIndex(
+	// 			text,
+	// 			start
+	// 		);
+	// 		const endPos = EditorExtensions.getEditorPositionFromIndex(
+	// 			text,
+	// 			end
+	// 		);
+
+	// 		editor.replaceRange(stackOverflowMarkdown, startPos, endPos);
+	// 	}
+	// }
+
+
+
+	async callOpenAIAPI(text: string, prompt: string) {
+		const response = await request({
+			url: 'https://api.openai.com/v1/engines/text-davinci-002/completions',
+			method: 'POST',
+			headers: {
+				'Authorization': `Bearer ${this.settings.apiKey}`,
+				'Content-Type': 'application/json'
+			},
+			contentType: 'application/json',
+			body: JSON.stringify({
+				"prompt": prompt,
+				"max_tokens": 250,
+				"temperature": 0.3,
+				"best_of": 3
+			})
+		});
+
+		const responseJSON = JSON.parse(response);
+		return responseJSON.choices[0].text;
+	}
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
+		
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+			id: 'summarize',
+			name: 'Summarize',
+			editorCallback: async (editor: Editor, view: MarkdownView) => {
+				// editor.replaceSelection('GPT3Summarizer Editor Command');
+				const text = editor.getSelection();
+				const summaryPrompt = `Summarize this text.\n\nText:\n${text}\n\nSummary:\n`
+				const summary = await this.callOpenAIAPI(editor.getSelection(), summaryPrompt);
+				const titlePrompt = `Suggest a one line title for the following text.\n\nText:\n${text}\n\nTitle:\n`
+				const title = await this.callOpenAIAPI(editor.getSelection(), titlePrompt);
+				editor.replaceSelection(`# ${title.trim()}\n\n## Summary:\n${summary}\n\n## Original Text:\n\n${editor.getSelection()}`);
 			}
 		});
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
@@ -56,7 +117,7 @@ export default class MyPlugin extends Plugin {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						new SampleModal(this.app).open();
+						new GPT3SummarizerModal(this.app).open();
 					}
 
 					// This command will only show up in Command Palette when the check function returns true
@@ -66,16 +127,7 @@ export default class MyPlugin extends Plugin {
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new GPT3SummarizerTab(this.app, this));
 	}
 
 	onunload() {
@@ -91,7 +143,7 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
+class GPT3SummarizerModal extends Modal {
 	constructor(app: App) {
 		super(app);
 	}
@@ -107,10 +159,10 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class GPT3SummarizerTab extends PluginSettingTab {
+	plugin: GPT3Summarizer;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: GPT3Summarizer) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -123,14 +175,14 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('OpenAI API Key')
+			.setDesc('API Key for OpenAI')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('some-api-key')
+				.setValue(this.plugin.settings.apiKey)
 				.onChange(async (value) => {
 					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.apiKey = value;
 					await this.plugin.saveSettings();
 				}));
 	}
